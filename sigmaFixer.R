e4f6aee65ece750ce2b76a256ecb007a98c19584
Michael Ghebre <michael_gh07@yahoo.co.uk>
2016-10-27 01:24:42 +0100

    add sigmaFixer.R

---
+++ sigmaFixer.R
@@ -0,0 +1,66 @@
+##################################
+# Michael A Ghebre, PhD          #
+# University of Leicester, Uk    #
+# Date 26 Oct 2016              #
+##################################
+# Variance covariance fixed 
+
+sigmaFixer <- function(sigma, ...){
+  sigma <- as.matrix(sigma)
+  r <- dim(sigma)[1]
+  fixrate <- 0.01 
+  covfixmat <- array(1,c(r,r)) + fixrate*diag(1,r)
+  min_limit <- .Machine$double.eps*10
+  
+  if (!all(is.finite(sigma))){
+    warning("covariance matrix is not finite")
+  }
+  
+  # Enforces squareness and symmetricity   
+  nsigma <- sigma - ((sigma - t(sigma))/2)
+  iter <- 0
+  
+  # Checking the covariance matrix is not positive definite
+  while (postDef(nsigma) == 0 & (iter < 10000)){
+    
+    iter <- iter + 1
+    d <- diag(nsigma)
+    if (any(d <= min_limit)){
+      m <- max(abs(d))*fixrate
+      neg <- min(d)
+      
+      if (neg < 0){
+        addit <- (m - neg)*diag(1,r)
+      }
+      else {
+        if (m < min_limit){
+          m <- min_limit 
+        } 
+        addit <- m*diag(1,r)
+      } 
+      nsigma <- nsigma + addit
+    }
+    else {
+      # Increase the diagonal values by 1%       
+      nsigma <- nsigma*covfixmat 
+    }
+  }
+  # return(list(nsigma, iter))
+  return(nsigma)   
+} 
+
+
+#testing for postive definite 
+postDef <- function(Sigma){
+  Sigma <- as.matrix(Sigma)
+  q <- try(chol(Sigma)[2], TRUE)
+  if (q==0){
+    t <- 1
+  }
+  else {
+    t <- 0
+  }
+  return(t)
+}
+
+
